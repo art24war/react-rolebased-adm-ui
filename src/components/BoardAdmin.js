7b887d7e361a72a306e5b583bedffbbf8e7df3a4
artwar <art24war@live.com>
2021-03-29 23:10:39 +0300

    fix reload user request by relogin with  refresh_token

--- src/components/BoardAdmin.js
+++ src/components/BoardAdmin.js
@@ -14,10 +14,6 @@ const BoardAdmin = () => {
         setContent(response.data);
       },
       (error) => {
-        if(error.status == 401)
-        {
-          history.push("/login");
-        }
         const _content =
           (error.response &&
             error.response.data &&
@@ -25,7 +21,7 @@ const BoardAdmin = () => {
           error.message ||
           error.toString();
 
-        setContent(_content);
+        console.log(_content);
       }
     );
   }, []);
